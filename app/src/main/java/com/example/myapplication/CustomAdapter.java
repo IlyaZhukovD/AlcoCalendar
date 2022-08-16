3a9206d73b64b5d445d4249fe9211e2ce0a01cc9
ilya <ilysha022@yandex.com>
2022-08-16 13:32:54 +0300

    popa

--- app/src/main/java/com/example/myapplication/CustomAdapter.java
+++ app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -31,6 +31,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
         public TextView getTextView()
         {
+            String popa = "popa";
             String none = "none";
             return textView;
         }
